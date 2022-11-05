506986145ce776f887a74c67edce62a08ee58a24
kazi-kabir <kazi.s221999@gmail.com>
2022-11-05 12:30:54 +1100

    give up for today

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,6 @@ import cheerio from 'cheerio';
 // const url = 'https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1'; // URL we're scraping
 const AxiosInstance = axios.create(); // Create a new Axios Instance
 
-interface PlayerData {
-    rank: number; // 1 - 20 rank
-    name: string;
-    goals: number;
-  }
-
 const url = 'https://en.wikipedia.org/wiki/List_of_unicorn_startup_companies'
 
 interface CompanyData {
@@ -24,7 +18,7 @@ AxiosInstance.get(url)
     response => {
         const html = response.data; 
         const $ = cheerio.load(html); 
-        const statsTable: cheerio.Cheerio = $('.wikitable sortable jquery-tablesorter > tr'); 
+        const statsTable: cheerio.Cheerio = $('.wikitable sortable jquery-tablesorter tbody tr'); 
         console.log(statsTable); 
         const topCompanies: CompanyData[] = [];
 
@@ -40,7 +34,7 @@ AxiosInstance.get(url)
         //       country
         //     })
         // })
-        console.log(topCompanies) 
+        // console.log(topCompanies) 
     }
   )
   .catch(console.error); 
