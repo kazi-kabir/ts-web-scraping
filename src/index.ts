a22d27158665ff14600ccdc6b474c0913b7c5575
kazi-kabir <kazi.s221999@gmail.com>
2022-11-10 21:30:29 +1100

    trying to store in array

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 
-
-
 const url = 'https://en.wikipedia.org/wiki/List_of_unicorn_startup_companies'; 
 const AxiosInstance = axios.create(); 
-
 
 interface CompanyData {
     details: string;
@@ -18,12 +15,15 @@ AxiosInstance.get(url)
         const $ = cheerio.load(html); 
         const statsTable: cheerio.Cheerio = $('.wikitable tbody > tr');
         const topCompanies: CompanyData[] = [];
+        let arr = [];
 
         statsTable.each((i, elem) => {
             let details: string = $(elem).find('td').text(); 
             details = details.split(' ').join('');
-            console.log(details);
-            })
-        }
+            arr[i] = details;
+            arr.length = 10;
+        })
+        arr.forEach(n => console.log(n))
+    }
   )
   .catch(console.error); 
