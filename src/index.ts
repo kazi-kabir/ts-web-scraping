ee7a4944934d66dbd3c19bf144de37325893d1b7
kazi-kabir <kazi.s221999@gmail.com>
2022-11-16 00:02:58 +1100

    extracted cleanData into separate utils file

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,9 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
+import { cleanData } from './utils';
 
 const url = 'https://en.wikipedia.org/wiki/List_of_unicorn_startup_companies'; 
 const AxiosInstance = axios.create(); 
-
-interface CompanyData {
-    company: string,
-    valuation: number,
-    country: string
-    founders: string,
-}
-
-const cleanData = (topCompanies: string[]): CompanyData[] => {
-    // manipulating data directly, bad practice
-
-    topCompanies.length = 10;
-    
-    let unacceptableStrings: string[] = ['CBInsights', 'VentureBeat', 'TechCrunch', 'Incmagazine'];
-
-    let filterOutEmpty = topCompanies
-    .filter(company => company.length > 1);
-    
-    let filterOutCBInsights = filterOutEmpty
-    .filter(company => !company.includes('CBInsights'));
-
-    let filterOutTechCrunch = filterOutCBInsights
-    .filter(company => !company.includes('TechCrunch'));
-
-    let filterOutIncMagazine = filterOutTechCrunch
-    .filter(company => !company.includes('Incmagazine'));
-
-    console.log(filterOutIncMagazine);
-    return [];
-}
-
 
 AxiosInstance.get(url)
   .then( 
---
+++ src/utils.ts
@@ -0,0 +1,30 @@
+interface CompanyData {
+    company: string,
+    valuation: number,
+    country: string
+    founders: string,
+}
+
+export const cleanData = (topCompanies: string[]): String[]  => {
+    topCompanies.length = 20;
+
+    let unacceptableStrings: string[] = ['CBInsights', 'VentureBeat', 'TechCrunch', 'Incmagazine'];
+
+    let filterOutEmpty = topCompanies
+    .filter(company => company.length > 1);
+
+    let filterOutCBInsights = filterOutEmpty
+    .filter(company => !company.includes('CBInsights'));
+
+    let filterOutTechCrunch = filterOutCBInsights
+    .filter(company => !company.includes('TechCrunch'));
+
+    let filterOutIncMagazine = filterOutTechCrunch
+    .filter(company => !company.includes('Incmagazine'));
+
+    let filterOutVentureBeat = filterOutIncMagazine
+    .filter(company => !company.includes('VentureBeat'));
+
+    console.log(filterOutVentureBeat);
+    return [];
+};
