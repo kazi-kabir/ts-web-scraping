ab199beab77d0af6e598b4fadb11042a61ee3f2c
kazi-kabir <kazi.s221999@gmail.com>
2022-11-19 13:55:31 +1100

    works now in postman

--- src/index.ts
+++ src/index.ts
@@ -29,4 +29,22 @@ AxiosInstance.get(url)
 )
 .catch(console.error); 
 
-export default companyDataStore;
+const express = require('express');
+const app = express();
+const port = 3050;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+});
+
+app.get('/companyData', (req, res) => {
+    res.json(JSON.stringify(companyDataStore));
+})
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
